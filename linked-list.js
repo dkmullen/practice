f1dc0dc2ae5ba5121fa1f3c9c9bb444d1babbfb0
Dennis Mullen <dkmullen@gmail.com>
2018-02-10 10:42:06 -0500

    Add more notes to linked list section

--- linked-list.js
+++ linked-list.js
@@ -9,3 +9,39 @@ const nodeTwo = {
 };
 
 nodeOne.next = nodeTwo;
+
+/*
+The above produces: { data: 123, next: { data: 456 } }
+*/
+
+console.log(nodeOne);
+
+/*
+Link list: Ordered collection of data, made up of nodes. Each node contains some
+data plus a reference to the next node.
+
+Head node: First in list
+Tail node: Last, and can be identified by the fact that it has no ref to next node
+*/
+
+class Node {
+  // By convention, setting default of next to null rather than letting it be
+  // undefined indicates we intentionally wanted to default to null
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  // Func to insert data in the head position, pushing other data (if any) down the chain
+  insertFirst(data) { //Before this function runs,
+    // this.head refers a previously-created node. Our function below creates a
+    // new node, inserts it into the head position, and then the previous this.head
+    // becomes 'next' as per the Node constructor, above
+    this.head = new Node(data, this.head);
+  }
+}
