10059c04d9f4477bf84cc88be2b8817810768fa2
Dennis Mullen <dkmullen@gmail.com>
2018-02-23 20:12:34 -0500

    Add a validate function for binary search tree

--- tree.js
+++ tree.js
@@ -171,3 +171,32 @@ class Node2
   }
 
 }
+
+// --- Directions
+// Given a node, validate the binary search tree,
+// ensuring that every node's left hand child is
+// less than the parent node's value, and that
+// every node's right hand child is greater than
+// the parent
+
+function validate(node, min = null, max = null) {
+  // Skip these first two the first time thur
+  if (max !== null && node.data > max) {
+    return false;
+  }
+
+  if (min !== null && node.data < min) {
+    return false;
+  }
+
+  // If there is a node.left and node.left won't validate with max of node.data...
+  if (node.left && !validate(node.left, min, node.data)) {
+    return false;
+  }
+
+  if (node.right && !validate(node.right, node.data, max)) {
+    return false;
+  }
+
+  return true;
+}
